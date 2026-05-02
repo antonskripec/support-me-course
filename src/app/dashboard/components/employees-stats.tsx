c2cc14b4a7c553b9ae20d397858677387cbea10e
Anton Skripec <anton.skripec@hotmail.com>
2026-05-02 18:27:42 +0200

    Refactor EmployeesStats component to display employee statistics using Card components

--- src/app/dashboard/components/employees-stats.tsx
+++ src/app/dashboard/components/employees-stats.tsx
@@ -1,8 +1,33 @@
+import
+    {
+        Card,
+        CardDescription,
+        CardHeader,
+        CardTitle,
+    } from "@/components/ui/card"
+
 export default function EmployeesStats()
 {
     return (
-        <div>
-            Employees stats
+        <div className="grid lg:grid-cols-3 gap-4">
+            <Card>
+                <CardHeader>
+                    <CardDescription>Total employees</CardDescription>
+                    <CardTitle>0</CardTitle>
+                </CardHeader>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardDescription>Active employees</CardDescription>
+                    <CardTitle>0</CardTitle>
+                </CardHeader>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardDescription>Inactive employees</CardDescription>
+                    <CardTitle>0</CardTitle>
+                </CardHeader>
+            </Card>
         </div>
     )
 }

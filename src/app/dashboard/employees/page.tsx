89950732c2cb5b6e08672d0e0a973c8cb1726f2a
Anton Skripec <anton.skripec@hotmail.com>
2026-05-02 16:24:07 +0200

    Add Account, Employees, Settings, and Teams pages with basic structure

---
+++ src/app/dashboard/employees/page.tsx
@@ -0,0 +1,6 @@
+export default function EmployeesPage()
+{
+    return (
+        <div>Employees Page</div>
+    )
+}
---
+++ src/app/dashboard/teams/page.tsx
@@ -0,0 +1,6 @@
+export default function TeamsPage()
+{
+    return (
+        <div>Teams Page</div>
+    )
+}
